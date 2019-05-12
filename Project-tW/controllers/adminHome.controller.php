94e3e72960c8749d58d4b7a9c2661e3f294517ce
nuyonu <nuyonu@gmail.com>
2019-05-12 15:12:26 +0300

    Adaugat template-uri pentru paginile adminului
    -Am adaugat un template care va fi folosit de restul echipei pentru a implementa functionalitatea adminului.

---
+++ Project-tW/controllers/adminHome.controller.php
@@ -0,0 +1,8 @@
+<?php
+class adminHomeController extends Controller
+{
+    public function show()
+    {
+        require_once (ROOT . DS . 'views' . DS . 'admin-home.php');
+    }
+}
---
+++ Project-tW/controllers/adminTrainings.controller.php
@@ -0,0 +1,8 @@
+<?php
+class adminTrainingsController extends Controller
+{
+    public function show ()
+    {
+        require_once (ROOT . DS . 'views' . DS . 'admin-trainings.php');
+    }
+}
